1ed8fa1df997bfad8b0ba844bcb39b761dc6d313
Derek Smale <dereksmale2@gmail.com>
2020-11-08 18:52:00 -0500

    fix delete error and habit detail error

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from datetime import date
 from django.contrib.auth.decorators import login_required
 from .models import Habit, Record
 from .forms import HabitForm, RecordForm, SearchForm
